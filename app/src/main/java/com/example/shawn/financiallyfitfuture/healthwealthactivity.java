6bb4ff7f6b525de4578e131cd3d11b3e19fe96f1
Siddharth Kumar <sidkumar@live.com>
2018-09-20 16:21:57 -0500

    stuff

--- app/src/main/java/com/example/shawn/financiallyfitfuture/healthwealthactivity.java
+++ app/src/main/java/com/example/shawn/financiallyfitfuture/healthwealthactivity.java
@@ -1,16 +1,19 @@
 package com.example.shawn.financiallyfitfuture;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 public class healthwealthactivity extends AppCompatActivity {
 
     private TextView mTextMessage;
-
+    private FloatingActionButton fab;
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
@@ -37,6 +40,15 @@ public class healthwealthactivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //launch unity
+                Intent i = new Intent(getApplicationContext(), AR_Activity.class);
+                startActivity(i);
+            }
+        });
     }
 
 }
