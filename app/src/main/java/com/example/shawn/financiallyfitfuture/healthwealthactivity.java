a0cbf576840ed206bba5a8766dbcbaf1313889da
Siddharth Kumar <sidkumar@live.com>
2018-09-20 17:09:47 -0500

    json

--- app/src/main/java/com/example/shawn/financiallyfitfuture/healthwealthactivity.java
+++ app/src/main/java/com/example/shawn/financiallyfitfuture/healthwealthactivity.java
@@ -1,14 +1,25 @@
 package com.example.shawn.financiallyfitfuture;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.util.JsonReader;
+import java.util.*;
+import android.util.*;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class healthwealthactivity extends AppCompatActivity {
 
@@ -50,9 +61,48 @@ public class healthwealthactivity extends AppCompatActivity {
             }
         });
 
-        String s = 
+       // mTextMessage.setText(LoadJSON(getApplicationContext()));
+        JSONObject master = parseJSON(LoadJSON(getApplicationContext()));
 
 
+
+    }
+    public String LoadJSON(Context context){
+        String json = null;
+        try {
+            InputStream is = context.getAssets().open("challenges.json");
+
+            int size = is.available();
+
+            byte[] buffer = new byte[size];
+
+            is.read(buffer);
+
+            is.close();
+
+            json = new String(buffer, "UTF-8");
+
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        return json;
+    }
+
+    public JSONObject parseJSON(String s){
+        JSONObject json = null;
+        try{
+            json = new JSONObject(s);
+        }
+        catch(JSONException e){
+            e.printStackTrace();
+            return null;
+        }
+
+
+
+        return json;
     }
 
 }
