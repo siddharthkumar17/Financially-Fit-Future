21590abb1eac21d01958a75809ec5a8c2b652532
Siddharth Kumar <sidkumar@live.com>
2018-09-21 01:46:22 -0500

    arcore

--- app/src/main/java/com/example/shawn/financiallyfitfuture/AR_Activity.java
+++ app/src/main/java/com/example/shawn/financiallyfitfuture/AR_Activity.java
@@ -4,8 +4,15 @@ import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.Node;
+import com.google.ar.sceneform.math.Vector3;
 import com.google.ar.sceneform.rendering.*;
 import com.google.ar.sceneform.ux.*;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.Random;
+import java.util.concurrent.ExecutionException;
 
 public class AR_Activity extends AppCompatActivity {
    // private static final double MIN_OPENGL_VERSION = 3.0;
@@ -21,7 +28,29 @@ public class AR_Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ar_);
         arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.ar_fragment);
+        arFragment.getPlaneDiscoveryController().hide();
+        arFragment.getPlaneDiscoveryController().setInstructionView(null);
 
+        AnchorNode node = new AnchorNode();
+        node.setParent(arFragment.getArSceneView().getScene());
+        Random random = new Random();
+
+        //node.setLocalPosition(new Vector3(5,5, 5 ));
+
+        ViewRenderable.builder().setView(this, R.layout.test_view).build()
+                .thenAccept(viewRenderable -> {
+                    Node noteText = new Node();
+                    noteText.setParent(arFragment.getArSceneView().getScene());
+                    //noteText.setParent(postitNode);
+                    noteText.setRenderable(viewRenderable);
+                    
+                    //set the note position in relation to its parent node.
+                    // In this case, along the y axis of the Post-It note
+                    noteText.setWorldPosition(new Vector3(0.5f, -0.05f, 0f));
+                });
+            //node.setRenderable(ViewRenderable.builder().setView(getApplicationContext(), R.layout.test_view).build().join());
+
+        // CompletableFuture<ViewRenderable> future = ViewRenderable.builder().setView(getApplicationContext(), R.layout.test_view).build();
 
 
 
