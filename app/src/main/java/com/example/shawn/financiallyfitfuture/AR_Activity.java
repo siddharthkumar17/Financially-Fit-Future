720b57ae6bd0959a8fbb22a3badd4023edd69745
Siddharth Kumar <sidkumar@live.com>
2018-09-21 04:04:48 -0500

    delayed spawn finished

--- app/src/main/java/com/example/shawn/financiallyfitfuture/AR_Activity.java
+++ app/src/main/java/com/example/shawn/financiallyfitfuture/AR_Activity.java
@@ -1,6 +1,7 @@
 package com.example.shawn.financiallyfitfuture;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -37,17 +38,7 @@ public class AR_Activity extends AppCompatActivity {
 
         //node.setLocalPosition(new Vector3(5,5, 5 ));
 
-        ViewRenderable.builder().setView(this, R.layout.test_view).build()
-                .thenAccept(viewRenderable -> {
-                    Node noteText = new Node();
-                    noteText.setParent(arFragment.getArSceneView().getScene());
-                    //noteText.setParent(postitNode);
-                    noteText.setRenderable(viewRenderable);
-                    
-                    //set the note position in relation to its parent node.
-                    // In this case, along the y axis of the Post-It note
-                    noteText.setWorldPosition(new Vector3(0.5f, -0.05f, 0f));
-                });
+        new SpawnTask().execute();
             //node.setRenderable(ViewRenderable.builder().setView(getApplicationContext(), R.layout.test_view).build().join());
 
         // CompletableFuture<ViewRenderable> future = ViewRenderable.builder().setView(getApplicationContext(), R.layout.test_view).build();
@@ -55,6 +46,41 @@ public class AR_Activity extends AppCompatActivity {
 
 
 
+    }
+    public void spawn(){
+        ViewRenderable.builder().setView(this, R.layout.test_view).build()
+                .thenAccept(viewRenderable -> {
+                    Node noteText = new Node();
+                    noteText.setParent(arFragment.getArSceneView().getScene());
+                    //noteText.setParent(postitNode);
+                    noteText.setRenderable(viewRenderable);
+
+                    //set the note position in relation to its parent node.
+                    // In this case, along the y axis of the Post-It note
+                    noteText.setWorldPosition(new Vector3(0.5f, -0.05f, 0f));
+                    // noteText.setWorldRotation();
+                });
+    }
+    class SpawnTask extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... voids) {
+           // while(true) {
+                Random random = new Random();
+                try {
+                    Thread.sleep(random.nextInt(5000) + 10000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                    return null;
+                }
+
+           // }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            spawn();
+        }
     }
 
 
---
+++ app/src/main/java/com/example/shawn/financiallyfitfuture/SpawnTask.java
@@ -0,0 +1,12 @@
+package com.example.shawn.financiallyfitfuture;
+
+import android.os.AsyncTask;
+
+import java.util.function.Function;
+
+public class SpawnTask extends AsyncTask<Void, Void, Void> {
+    @Override
+    protected Void doInBackground(Void... voids) {
+        return null;
+    }
+}
