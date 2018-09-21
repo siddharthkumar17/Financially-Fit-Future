ed057f1d675070badec3141d3756ca78a4769f9b
Siddharth Kumar <sidkumar@live.com>
2018-09-21 00:09:52 -0500

    arcore

---
+++ app/src/main/java/com/example/shawn/financiallyfitfuture/ARFragment.java
@@ -0,0 +1,10 @@
+package com.example.shawn.financiallyfitfuture;
+
+import android.app.Fragment;
+
+public class ARFragment extends Fragment {
+
+
+
+
+}
--- app/src/main/java/com/example/shawn/financiallyfitfuture/AR_Activity.java
+++ app/src/main/java/com/example/shawn/financiallyfitfuture/AR_Activity.java
@@ -4,11 +4,33 @@ import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class AR_Activity extends Activity {
+import com.google.ar.sceneform.rendering.*;
+import com.google.ar.sceneform.ux.*;
+
+public class AR_Activity extends AppCompatActivity {
+    private static final double MIN_OPENGL_VERSION = 3.0;
+
+    private ArFragment arFragment;
+    private ModelRenderable andyRenderable;
+
+
+
 
     @Override
+    @SuppressWarnings({"AndroidApiChecker", "FutureReturnValueIgnored"})
+    // CompletableFuture requires api level 24
+    // FutureReturnValueIgnored is not valid
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ar_);
+        arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.ar_fragment);
+
+
+
+
+
     }
+
+
 }
